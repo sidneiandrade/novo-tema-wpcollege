feb14da81575ff07ab595df54060badeeb0b42b1
Sidnei Andrade <contato@sadigital.com.br>
2016-12-05 22:19:03 -0300

    adicionando box de promoções de hospedagem e deletando imagens antigas

--- pagina-principal.php
+++ pagina-principal.php
@@ -64,20 +64,93 @@
   <?php endwhile; else: endif; ?>
 <!-- Fecha Ultimas Postagens -->
 
-<!-- Box Promoções e Templates -->
+<!-- Box Hospedagens e Templates -->
 <div class="row">
   <div class="container">
+
+<!-- Abre Box Promoções de Hospedagem -->
     <div class="col-md-6">
       <h1>Promoções de Hospedagem</h1>
       <p>promoções de hospedagem aqui</p>
+      
+      <div class="media"> 
+        <div class="media-left">
+          <a href="#">
+            <img class="media-object" src="<?php echo get_stylesheet_directory_uri(); ?>/img/host1plus-afiliados.png" alt="Promoções de Hospedagem">
+          </a>
+        </div>
+        <div class="media-body">
+          <h3 class="media-heading">Media heading</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio natus dolorem veniam laudantium eius sed libero dolore, dignissimos, et aspernatur quam,</p>
+          <a class="btn btn-success" href="#" role="button">Quero a Promoção</a>
+        </div>
+      </div>
+
+      <div class="media"> 
+      <div class="media-left">
+        <a href="#">
+          <img class="media-object" src="<?php echo get_stylesheet_directory_uri(); ?>/img/host1plus-afiliados.png" alt="Promoções de Hospedagem">
+        </a>
+      </div>
+      <div class="media-body">
+        <h3 class="media-heading">Media heading</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio natus dolorem veniam laudantium eius sed libero dolore, dignissimos, et aspernatur quam,</p>
+        <a class="btn btn-success" href="#" role="button">Quero a Promoção</a>
+      </div>
+      </div>
+
+      <div class="media"> 
+      <div class="media-left">
+        <a href="#">
+          <img class="media-object" src="<?php echo get_stylesheet_directory_uri(); ?>/img/host1plus-afiliados.png" alt="Promoções de Hospedagem">
+        </a>
+      </div>
+      <div class="media-body">
+        <h3 class="media-heading">Media heading</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio natus dolorem veniam laudantium eius sed libero dolore, dignissimos, et aspernatur quam,</p>
+        <a class="btn btn-success" href="#" role="button">Quero a Promoção</a>
+      </div>
+      </div>
+
+      <div class="media"> 
+      <div class="media-left">
+        <a href="#">
+          <img class="media-object" src="<?php echo get_stylesheet_directory_uri(); ?>/img/host1plus-afiliados.png" alt="Promoções de Hospedagem">
+        </a>
+      </div>
+      <div class="media-body">
+        <h3 class="media-heading">Media heading</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio natus dolorem veniam laudantium eius sed libero dolore, dignissimos, et aspernatur quam,</p>
+        <a class="btn btn-success" href="#" role="button">Quero a Promoção</a>
+      </div>
+      </div>
+
+      <div class="call-action-postagem">
+        <p>Veja mais promoções de hospedagem clicando no botão abaixo.</p>
+        <a class="btn btn-primary" href="#" role="button">Mais Promoções</a>
+      </div>
+
     </div>
+<!-- Fecha Box Promoções de Hospedagem -->
+
+<!-- Abre Box Promoções de Templates & Plugins -->
+
     <div class="col-md-6">
       <h1>Promoções de Templates e Plugins</h1>
       <p>Promoções de temas e plugins aqui</p>
+    
+
+
+
+
     </div>
+
+<!-- Fecha Box Promoções de Templates & Plugins -->
+
+
   </div>
 </div>
-<!-- Box Promoções e Templates -->
+<!-- Box Hospedagens e Templates -->
 
 <!-- Criação do site -->
 <?php get_footer(); ?>
